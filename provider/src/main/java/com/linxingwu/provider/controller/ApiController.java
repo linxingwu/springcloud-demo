7cc8a2b112e60d03f5af1f98152dc6f0c5bbacab
linxingwu <linxingwu@hotmail.com>
2018-09-29 20:04:08 +0800

    provider配置文件失效

--- provider/src/main/java/com/linxingwu/provider/controller/ApiController.java
+++ provider/src/main/java/com/linxingwu/provider/controller/ApiController.java
@@ -2,22 +2,25 @@ package com.linxingwu.provider.controller;
 
 import com.linxingwu.entity.User;
 import com.linxingwu.service.HelloService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController implements HelloService {
     @Override
-    public String hello(String name) {
+    public String hello(@RequestParam String name) {
         return "Hello "+name;
     }
 
     @Override
-    public User hello(String name, Integer age) {
+    public User hello(@RequestHeader String name, @RequestHeader Integer age) {
         return new User(name,age);
     }
 
     @Override
-    public String hello(User user) {
+    public String hello(@RequestBody User user) {
         return "Hello "+user.getName()+" you are "+user.getAge()+" years old";
     }
 }
