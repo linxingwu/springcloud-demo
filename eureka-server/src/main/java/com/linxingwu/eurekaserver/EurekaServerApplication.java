1878604b506c080393885b7a3d7ec1436dbcffcd
linxingwu <liam.lin@jollycorp.com>
2018-10-22 14:04:02 +0800

    线程中断

--- eureka-server/src/main/java/com/linxingwu/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/linxingwu/eurekaserver/EurekaServerApplication.java
@@ -4,11 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.concurrent.*;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+//        SpringApplication.run(EurekaServerApplication.class, args);
+        LinkedBlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>();
+        SynchronousQueue<Runnable> sq = new SynchronousQueue<>();
+        ThreadFactory threadFactory = r -> {
+            Thread thread = new Thread(r);
+            thread.setName("");
+            return thread;
+        };
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(2,4,100L, TimeUnit.MILLISECONDS,queue,threadFactory);
+
+        for (int i = 0;;i++){
+            System.out.println("running"+i);
+            Thread.sleep(1000);
+        }
+
     }
 }
