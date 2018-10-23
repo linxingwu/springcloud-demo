dad9fc9cfa10fd5ba5a88bb2075297fa50ac6bef
linxingwu <linxingwu@hotmail.com>
2018-10-23 20:41:43 +0800

    网关过滤器功能

--- gateway/src/main/java/com/linxingwu/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/linxingwu/gateway/GatewayApplication.java
@@ -1,8 +1,10 @@
 package com.linxingwu.gateway;
 
+import com.linxingwu.gateway.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -10,5 +12,10 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public AccessFilter getFilter(){
+        return new AccessFilter();
     }
 }
---
+++ gateway/src/main/java/com/linxingwu/gateway/filter/AccessFilter.java
@@ -0,0 +1,46 @@
+package com.linxingwu.gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+public class AccessFilter extends ZuulFilter {
+
+    private static Logger logger = LoggerFactory.getLogger(AccessFilter.class);
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        logger.info("send {} request to {}",request.getMethod(),request.getRequestURL().toString());
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null){
+            logger.warn("access token is empty");
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(403);
+            context.setResponseBody("403 Forbidden");
+            return null;
+        }
+        logger.info("access token ok");
+        return null;
+    }
+}
