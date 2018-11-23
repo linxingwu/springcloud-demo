eade3a150429db7ba1d7c003bcdb25d9b16de11e
linxingwu <linxingwu@hotmail.com>
2018-11-23 16:45:14 +0800

    动态路由不生效

--- gateway/src/main/java/com/linxingwu/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/linxingwu/gateway/GatewayApplication.java
@@ -3,8 +3,12 @@ package com.linxingwu.gateway;
 import com.linxingwu.gateway.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -17,5 +21,12 @@ public class GatewayApplication {
     @Bean
     public AccessFilter getFilter(){
         return new AccessFilter();
+    }
+    @Bean
+    @Primary
+    @RefreshScope
+    @ConfigurationProperties("zuul")
+    public ZuulProperties zuulProperties(){
+        return new ZuulProperties();
     }
 }
