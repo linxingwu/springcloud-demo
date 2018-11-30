4a33c90d404a13934808a90bb77e2bd445adec79
linxingwu <linxingwu@hotmail.com>
2018-11-30 10:35:34 +0800

    stream 具体通道的异常处理

--- stream/src/main/java/com/linxingwu/stream/receiver/SinkProcessor.java
+++ stream/src/main/java/com/linxingwu/stream/receiver/SinkProcessor.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.SendTo;
 
@@ -18,6 +19,11 @@ public class SinkProcessor {
     @SendTo({SinkSender.OUTPUT})
     public Object receive(Message<String> value){
         logger.info("Received:\t{}",value.getPayload());
+//        int i = 1/0;
         return "Do not answer\t"+value.getPayload()+"->处理过后的数据";
+    }
+    @ServiceActivator(inputChannel = SinkSender.INPUT+".serviceGroup.errors")
+    public void error(Message message){
+        logger.error("Handing Error:\t{}",message);
     }
 }
--- stream2/src/test/java/com/linxingwu/stream/StreamApplicationTests.java
+++ stream2/src/test/java/com/linxingwu/stream/StreamApplicationTests.java
@@ -1,22 +1,22 @@
 package com.linxingwu.stream;
 
-import com.linxingwu.stream.receiver.SinkSender;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@EnableBinding({SinkSender.class})
+@EnableBinding({Processor.class})
 public class StreamApplicationTests {
 
     @Autowired
-    private SinkSender sender;
+    private Processor sender;
 
     @Test
     public void contextLoads() throws InterruptedException {
