3291a1304d4144f89c9768668278c3610f624977
linxingwu <linxingwu@hotmail.com>
2018-12-13 11:58:55 +0800

    eureka httpbasic 方式开启加密 但是config加密后client无法读取配置

--- eureka-server/src/main/java/com/linxingwu/eurekaserver/config/WebSecurity.java
+++ eureka-server/src/main/java/com/linxingwu/eurekaserver/config/WebSecurity.java
@@ -1,15 +1,14 @@
-package com.linxingwu.config.config;
-
+package com.linxingwu.eurekaserver.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        // 禁止csrf 改用 basic
+        http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
     }
 }
