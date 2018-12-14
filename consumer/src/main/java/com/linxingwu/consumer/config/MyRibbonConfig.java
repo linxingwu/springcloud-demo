2d8a8829d2231245c6f74c7295549cc047162fa4
linxingwu <linxingwu@hotmail.com>
2018-12-14 16:59:06 +0800

    ribbon feign 自定义配置 修复自定义LoadBalancer 导致找不到server异常

--- consumer/src/main/java/com/linxingwu/consumer/config/MyRibbonConfig.java
+++ consumer/src/main/java/com/linxingwu/consumer/config/MyRibbonConfig.java
@@ -1,15 +1,29 @@
 package com.linxingwu.consumer.config;
 
-import com.netflix.loadbalancer.BaseLoadBalancer;
-import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+
 //在启动类中已经排除了这个类
 @Configuration
 public class MyRibbonConfig  {
+
     @Bean
-    public ILoadBalancer ribbonLoadBalancer(){
-        System.out.println("=============");
-        return new BaseLoadBalancer();
+    public ILoadBalancer ribbonLoadBalancer(IClientConfig config,
+                                            ServerList<Server> serverList, ServerListFilter<Server> serverListFilter,
+                                            IRule rule, IPing ping, ServerListUpdater serverListUpdater) {
+        return new MyLoadBalancer(config, rule, ping, serverList,
+                serverListFilter, serverListUpdater);
     }
+
+}
+
+//必须使用有参构造方法传入serverlist，否则调用找不到server报错
+class MyLoadBalancer extends DynamicServerListLoadBalancer{
+    MyLoadBalancer(IClientConfig config, IRule rule, IPing ping, ServerList<Server> serverList, ServerListFilter<Server> serverListFilter, ServerListUpdater serverListUpdater) {
+        super(config,rule,ping,serverList,serverListFilter,serverListUpdater);
+    }
+
 }
--- consumer/src/main/java/com/linxingwu/consumer/config/TestRibbonConfiguration.java
+++ consumer/src/main/java/com/linxingwu/consumer/config/TestRibbonConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Configuration;
 
 //本配置打开会导致找不到client
-//@Configuration
-//@RibbonClient(name="hello-service",configuration = MyRibbonConfig.class)
+@Configuration
+@RibbonClient(name="hello-service",configuration = MyRibbonConfig.class)
 public class TestRibbonConfiguration {
 }
